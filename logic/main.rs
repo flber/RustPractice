16e85d4f2fdf041c0f328c3ac170beb0ee0d8745
flber <benrhammond@gmail.com>
2020-10-30 17:20:57 -0700

    helloworld, logic, variables

---
+++ logic/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+	let n = 20;
+
+	if n < 30 {
+		println!("number is less than 30");
+	} else if n == 30 {
+		println!("number is 30");
+	} else {
+		println!("the number is greater than 30");
+	}
+}
---
+++ variables/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+	let fixed = 45;
+	println!("fixed: {}", fixed);
+
+	let mut mutable = 100;
+	println!("mutable: {}", mutable);
+	mutable = 20;
+	println!("mutable is now: {}", mutable);
+
+	let int_thirty_two: i32 = 45;
+	println!("a thirty-two bit integer: {}", int_thirty_two);
+}
